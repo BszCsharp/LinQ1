63577ff8c0efcd7ea54641652195b96e4c707a89
reinhold <reinhold@web.de>
2015-06-18 12:43:52 +0200

    mit Lamda

--- WindowsFormsLinQ/Form1.Designer.cs
+++ WindowsFormsLinQ/Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonBsp1 = new System.Windows.Forms.Button();
+            this.richTextBoxAusgabe = new System.Windows.Forms.RichTextBox();
+            this.buttonBsp2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonBsp1
+            // 
+            this.buttonBsp1.Location = new System.Drawing.Point(28, 12);
+            this.buttonBsp1.Name = "buttonBsp1";
+            this.buttonBsp1.Size = new System.Drawing.Size(123, 42);
+            this.buttonBsp1.TabIndex = 0;
+            this.buttonBsp1.Text = "Beispiel1";
+            this.buttonBsp1.UseVisualStyleBackColor = true;
+            this.buttonBsp1.Click += new System.EventHandler(this.buttonBsp1_Click);
+            // 
+            // richTextBoxAusgabe
+            // 
+            this.richTextBoxAusgabe.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxAusgabe.Location = new System.Drawing.Point(26, 234);
+            this.richTextBoxAusgabe.Name = "richTextBoxAusgabe";
+            this.richTextBoxAusgabe.Size = new System.Drawing.Size(563, 86);
+            this.richTextBoxAusgabe.TabIndex = 1;
+            this.richTextBoxAusgabe.Text = "";
+            // 
+            // buttonBsp2
+            // 
+            this.buttonBsp2.Location = new System.Drawing.Point(157, 12);
+            this.buttonBsp2.Name = "buttonBsp2";
+            this.buttonBsp2.Size = new System.Drawing.Size(123, 42);
+            this.buttonBsp2.TabIndex = 2;
+            this.buttonBsp2.Text = "Beispiel2";
+            this.buttonBsp2.UseVisualStyleBackColor = true;
+            this.buttonBsp2.Click += new System.EventHandler(this.buttonBsp2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 373);
+            this.ClientSize = new System.Drawing.Size(767, 373);
+            this.Controls.Add(this.buttonBsp2);
+            this.Controls.Add(this.richTextBoxAusgabe);
+            this.Controls.Add(this.buttonBsp1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "LinQ-Beispiele";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonBsp1;
+        private System.Windows.Forms.RichTextBox richTextBoxAusgabe;
+        private System.Windows.Forms.Button buttonBsp2;
     }
 }
 
--- WindowsFormsLinQ/Form1.cs
+++ WindowsFormsLinQ/Form1.cs
@@ -12,9 +12,49 @@ namespace WindowsFormsLinQ
 {
     public partial class Form1 : Form
     {
+        Person[] persons  = {
+                          new Person { Name = "Meier", Age = 34 },
+                          new Person { Name = "Müller", Age = 51 },
+                          new Person { Name = "Schmidt", Age = 30 },
+                          new Person { Name = "Fischer", Age = 25 },
+                          new Person { Name = "Schulz", Age = 67 },
+                        };
         public Form1()
         {
             InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var query = from pers in persons
+                        where pers.Age >= 50
+                        select pers; 
+        }
+
+        private void buttonBsp1_Click(object sender, EventArgs e)
+        {
+            //LinQ
+            persons[3].Age = 52;
+            var query = from pers in persons
+                        where pers.Age >= 50
+                        select pers;
+            richTextBoxAusgabe.Text = String.Empty;
+            foreach (var item in query)
+                richTextBoxAusgabe.Text += String.Format("{0,-15}{1}\n", item.Name, item.Age);
+            
+        }
+
+        private void buttonBsp2_Click(object sender, EventArgs e)
+        {
+            persons[3].Age = 53;
+            //Lambda-Expressions
+            var query = persons
+            .Where(p => p.Age >= 50);
+            //.Select(p => p);
+            richTextBoxAusgabe.Text = String.Empty;
+            foreach (var item in query)
+                richTextBoxAusgabe.Text += String.Format("{0,-15}{1}\n", item.Name, item.Age);
         }
     }
 }
